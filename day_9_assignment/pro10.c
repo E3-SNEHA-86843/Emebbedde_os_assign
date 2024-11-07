3628033f2fe0d57bb30f8c69c74be97be2af02d7
Snehakarande <snehakarande7020@gmail.com>
2024-11-07 18:41:20 +0530

    some changes are done

--- day_9_assignment/pro10.c
+++ day_9_assignment/pro10.c
@@ -5,22 +5,25 @@
 int main(){
 
 	int ret,cnt = 0,s;
-	while(cnt<5){
-	
+
+	for(int i=1;i<6;i++){	
 		ret = fork();
+		 cnt++;
 		if(ret == -1 ){
 		
 			printf("fork() failed\n");
 			_exit(0);
 		}else if(ret == 0){
-		
-			sleep(5);
-			_exit(0);
-		}else{
-		
-			cnt++;
+
 			printf("child count: %d\nchild with parent pid:%d\n",cnt,getppid());
+                        printf("child pid:%d\n",getpid());
+			waitpid(ret,&s,0);
+			_exit(0);
 		}
+		sleep(5);
+	}
+	for(int i=1;i<6;i++){
+	
 		waitpid(ret,&s,0);
 	}
 	return 0;
